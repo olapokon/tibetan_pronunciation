5c20a071a406718eb4d4430f735a7f8c59b5d3f0
A <aryjna1@gmail.com>
2019-09-12 14:18:52 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  superscripts,
+  prefixes,
+  roots,
+  suffixes,
+  subscripts
+} from "./tibetanUnicodeData";
+import "./App.css";
 
 function App() {
+  const [stateValues, setStateValues] = useState({
+    root: "",
+    superscript: "",
+    prefix: "",
+    suffix: "",
+    subscript: "",
+    availableAffixes: []
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setStateValues({ ...stateValues, [name]: value });
+    if (name === "root") {
+      setStateValues({ availableAffixes: [] });
+      handleRootChange(value);
+    }
+  }
+
+  function handleRootChange(value) {
+    /* make superscripts and prefixes available if the root belongs to the third or 
+    fourth column of the alphabet table, index 16 to the end of the array */
+    /* suffixes are available for all roots, specific subscripts are available
+    to specific roots */
+    if (roots.indexOf(value) && roots.indexOf(value) > 15) {
+      setStateValues({
+        availableAffixes: ["superscript", "prefix"]
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {"\u0F66\u0FA8\u0FB2"} */}
+      <strong>Superscript </strong>
+      <select
+        name="superscript"
+        value={stateValues.superscript}
+        onChange={handleChange}
+      >
+        <option></option>
+        {stateValues.availableAffixes.includes("superscript") &&
+          superscripts.map((superscript, index) => (
+            <option key={index}>{superscript}</option>
+          ))}
+      </select>
+      <br></br>
+      <strong>Prefix </strong>
+      <select name="prefix" value={stateValues.prefix} onChange={handleChange}>
+        <option></option>
+        {stateValues.availableAffixes.includes("prefix") &&
+          prefixes.map((prefix, index) => (
+            <option key={index}>{prefix}</option>
+          ))}
+      </select>
+      <strong>Root syllable </strong>
+      <select name="root" value={stateValues.root} onChange={handleChange}>
+        <option></option>
+        {roots.map((root, index) => (
+          <option key={index}>{root}</option>
+        ))}
+      </select>
+      <strong>Suffix </strong>
+      <select name="suffix" value={stateValues.suffix} onChange={handleChange}>
+        <option></option>
+        {suffixes.map((suffix, index) => (
+          <option key={index}>{suffix}</option>
+        ))}
+      </select>
+      <br></br>
+      <strong>Subscript </strong>
+      <select
+        name="subscript"
+        value={stateValues.subscript}
+        onChange={handleChange}
+      >
+        <option></option>
+        {subscripts.map((subscript, index) => (
+          <option key={index}>{subscript}</option>
+        ))}
+      </select>
     </div>
   );
 }
---
+++ src/tibetanUnicodeData.js
@@ -0,0 +1,47 @@
+export const superscripts = ["\u0F62", "\u0F63", "\u0F66"];
+export const prefixes = ["\u0F42", "\u0F51", "\u0F56", "\u0F58", "\u0F60"];
+export const roots = [
+  "\u0F40",
+  "\u0F45",
+  "\u0F4F",
+  "\u0F54",
+  "\u0F59",
+  "\u0F5E",
+  "\u0F62",
+  "\u0F67",
+  "\u0F41",
+  "\u0F46",
+  "\u0F50",
+  "\u0F55",
+  "\u0F5A",
+  "\u0F5F",
+  "\u0F63",
+  "\u0F68",
+  "\u0F42",
+  "\u0F47",
+  "\u0F51",
+  "\u0F56",
+  "\u0F5B",
+  "\u0F60",
+  "\u0F64",
+  "\u0F44",
+  "\u0F49",
+  "\u0F53",
+  "\u0F58",
+  "\u0F5D",
+  "\u0F61",
+  "\u0F66"
+];
+export const suffixes = [
+  "\u0F42",
+  "\u0F44",
+  "\u0F51",
+  "\u0F53",
+  "\u0F56",
+  "\u0F58",
+  "\u0F60",
+  "\u0F62",
+  "\u0F63",
+  "\u0F66"
+];
+export const subscripts = ["\u0F61", "\u0F62", "\u0F63"];
