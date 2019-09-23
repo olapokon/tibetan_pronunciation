2dedcd5db0439d6089987c1b4889c929dfc977f5
A <aryjna1@gmail.com>
2019-09-23 11:00:45 +0300

    working menus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import {
   superscripts,
   prefixes,
@@ -10,100 +10,119 @@ import "./App.css";
 
 // manual of standard tibetan p 44 -
 
-function App() {
-  const [stateValues, setStateValues] = useState({
-    root: "",
-    superscript: "",
-    prefix: "",
-    suffix: "",
-    subscript: "",
-    availableSubscripts: [],
-    availableAffixes: []
-  });
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      root: "",
+      superscript: "",
+      prefix: "",
+      suffix: "",
+      subscript: "",
+      availableSubscripts: [],
+      availableAffixes: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRootChange = this.handleRootChange.bind(this);
+  }
 
-  function handleChange(event) {
+  handleChange(event) {
     const { name, value } = event.target;
-    setStateValues({ ...stateValues, [name]: value });
     if (name === "root") {
-      setStateValues({ ...stateValues, availableAffixes: [] });
-      handleRootChange(value);
+      this.setState({
+        [name]: value,
+        availableAffixes: ["suffix"]
+      });
+      this.handleRootChange(value);
+    } else {
+      this.setState({ [name]: value });
     }
   }
 
-  function handleRootChange(value) {
+  handleRootChange(value) {
     /* make superscripts and prefixes available if the root belongs to the third or 
     fourth column of the alphabet table, index 16 to the end of the array */
     /* suffixes are available for all roots, specific subscripts are available
     to specific roots */
-    if (roots.indexOf(value) && roots.indexOf(value) > 15) {
-      setStateValues({
-        ...stateValues,
-        availableAffixes: ["superscript", "prefix"]
-      });
+    const affixes = ["suffix"];
+    if (roots.indexOf(value) > 15) {
+      affixes.push("prefix");
+      affixes.push("superscript");
     }
     if (subscriptsTable[value]) {
-      setStateValues({
-        ...stateValues,
-        availableAffixes: [...stateValues.availableAffixes, "subscript"],
+      affixes.push("subscript");
+      this.setState({
         availableSubscripts: [...subscriptsTable[value]]
       });
     }
+    this.setState({ availableAffixes: affixes });
+    console.log(affixes);
   }
 
-  return (
-    <div className="App">
-      {/* {"\u0F66\u0FA8\u0FB2"} */}
-      <strong>Superscript </strong>
-      <select
-        name="superscript"
-        value={stateValues.superscript}
-        onChange={handleChange}
-      >
-        <option></option>
-        {stateValues.availableAffixes.includes("superscript") &&
-          superscripts.map((superscript, index) => (
-            <option key={index}>{superscript}</option>
+  render() {
+    return (
+      <div className="App">
+        {/* {"\u0F66\u0FA8\u0FB2"} */}
+        <strong>Superscript </strong>
+        <select
+          name="superscript"
+          value={this.state.superscript}
+          onChange={this.handleChange}
+        >
+          <option></option>
+          {this.state.availableAffixes.includes("superscript") &&
+            superscripts.map((superscript, index) => (
+              <option key={index}>{superscript}</option>
+            ))}
+        </select>
+        <br></br>
+        <strong>Prefix </strong>
+        <select name="prefix" value={this.prefix} onChange={this.handleChange}>
+          <option></option>
+          {this.state.availableAffixes.includes("prefix") &&
+            prefixes.map((prefix, index) => (
+              <option key={index}>{prefix}</option>
+            ))}
+        </select>
+        <strong>Root syllable </strong>
+        <select
+          name="root"
+          value={this.state.root}
+          onChange={this.handleChange}
+        >
+          <option></option>
+          {roots.map((rootSyllable, index) => (
+            <option key={index}>{rootSyllable}</option>
           ))}
-      </select>
-      <br></br>
-      <strong>Prefix </strong>
-      <select name="prefix" value={stateValues.prefix} onChange={handleChange}>
-        <option></option>
-        {stateValues.availableAffixes.includes("prefix") &&
-          prefixes.map((prefix, index) => (
-            <option key={index}>{prefix}</option>
-          ))}
-      </select>
-      <strong>Root syllable </strong>
-      <select name="root" value={stateValues.root} onChange={handleChange}>
-        <option></option>
-        {roots.map((rootSyllable, index) => (
-          <option key={index}>{rootSyllable}</option>
-        ))}
-      </select>
-      <strong>Suffix </strong>
-      <select name="suffix" value={stateValues.suffix} onChange={handleChange}>
-        <option></option>
-        {stateValues.root &&
-          suffixes.map((suffix, index) => (
-            <option key={index}>{suffix}</option>
-          ))}
-      </select>
-      <br></br>
-      <strong>Subscript </strong>
-      <select
-        name="subscript"
-        value={stateValues.subscript}
-        onChange={handleChange}
-      >
-        <option></option>
-        {stateValues.availableAffixes.includes("subscript") &&
-          stateValues.availableSubscripts.map((subscript, index) => (
-            <option key={index}>{subscript}</option>
-          ))}
-      </select>
-    </div>
-  );
+        </select>
+        <strong>Suffix </strong>
+        <select
+          name="suffix"
+          value={this.state.suffix}
+          onChange={this.handleChange}
+        >
+          <option></option>
+          {this.state.availableAffixes.includes("suffix") &&
+            suffixes.map((suffix, index) => (
+              <option key={index}>{suffix}</option>
+            ))}
+        </select>
+        <br></br>
+        <strong>Subscript </strong>
+        <select
+          name="subscript"
+          value={this.state.subscript}
+          onChange={this.handleChange}
+        >
+          <option></option>
+          {this.state.availableAffixes.includes("subscript") &&
+            this.state.availableSubscripts.map((subscript, index) => (
+              <option key={index}>{subscript}</option>
+            ))}
+        </select>
+      </div>
+    );
+  }
 }
 
 export default App;
