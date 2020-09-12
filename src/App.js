5d44cdaf6927645094a9d547cee58de530c222e5
unknown <aryjna1@gmail.com>
2020-09-12 16:07:34 +0300

    Refactored to more switches

--- src/App.js
+++ src/App.js
@@ -140,64 +140,78 @@ class App extends Component {
 		}
 
 		if (this.state.subscript) {
-			// ra subscript
-			if (this.state.subscript === '\u0F62') {
-				if (
-					this.state.root === '\u0F40' ||
-					this.state.root === '\u0F4F' ||
-					this.state.root === '\u0F54'
-				) {
-					currentRoot = 'tra';
-					tone = 'high';
-				} else if (
-					this.state.root === '\u0F41' ||
-					this.state.root === '\u0F50' ||
-					this.state.root === '\u0F55'
-				) {
-					currentRoot = 'thra';
-					tone = 'high';
-				} else if (
-					this.state.root === '\u0F42' ||
-					this.state.root === '\u0F51' ||
-					this.state.root === '\u0F56'
-				) {
-					if (this.state.superscript === '\u0F66') {
-						currentRoot = 'dra';
-					} else {
-						currentRoot = 'thra';
+			switch (this.state.subscript) {
+				// ra subscript
+				case '\u0F62':
+					switch (this.state.root) {
+						case '\u0F40':
+						case '\u0F4F':
+						case '\u0F54':
+							currentRoot = 'tra';
+							tone = 'high';
+							break;
+						case '\u0F41':
+						case '\u0F50':
+						case '\u0F55':
+							currentRoot = 'thra';
+							tone = 'high';
+							break;
+						case '\u0F42':
+						case '\u0F51':
+						case '\u0F56':
+							if (this.state.superscript === '\u0F66') {
+								currentRoot = 'dra';
+							} else {
+								currentRoot = 'thra';
+							}
+							tone = 'low';
+							break;
+						case '\u0F67':
+							currentRoot = 'hra';
+							break;
+						default:
+							break;
 					}
-					tone = 'low';
-				} else if (this.state.root === '\u0F67') {
-					currentRoot = 'hra';
-				}
+					break;
 
 				// la subscript
-			} else if (this.state.subscript === '\u0F63') {
-				if (this.state.root === '\u0F5F') {
-					currentRoot = 'da';
-					tone = 'low';
-				} else {
-					currentRoot = 'la';
-					tone = 'high';
-				}
+				case '\u0F63':
+					if (this.state.root === '\u0F5F') {
+						currentRoot = 'da';
+						tone = 'low';
+					} else {
+						currentRoot = 'la';
+						tone = 'high';
+					}
+					break;
 
 				// ya subscript
-			} else if (this.state.subscript === '\u0F61') {
-				if (this.state.root === '\u0F58') {
-					currentRoot = 'nya';
-					tone = 'low';
-				} else if (this.state.root === '\u0F54') {
-					currentRoot = 'ca';
-					tone = 'high';
-				} else if (this.state.root === '\u0F55') {
-					currentRoot = 'cha';
-					tone = 'high';
-				} else if (this.state.root === '\u0F56') {
-					currentRoot = 'cha';
-					tone = 'low';
-				} else {
-					currentRoot = currentRoot.slice(0, -1) + 'ya';
-				}
+				case '\u0F61':
+					switch (this.state.root) {
+						case '\u0F58':
+							currentRoot = 'nya';
+							tone = 'low';
+							break;
+						case '\u0F54':
+							currentRoot = 'ca';
+							tone = 'high';
+							break;
+						case '\u0F55':
+							currentRoot = 'cha';
+							tone = 'high';
+							break;
+						case '\u0F56':
+							currentRoot = 'cha';
+							tone = 'low';
+							break;
+						default:
+							currentRoot = currentRoot.slice(0, -1) + 'ya';
+							break;
+					}
+					break;
+
+				default:
+					break;
 			}
 		}
 
