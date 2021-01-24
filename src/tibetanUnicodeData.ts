76f7193562891c47caa8cab6a27dcfd597a0bbd9
ak4ra <aryjna1@gmail.com>
2021-01-24 21:21:47 +0200

    tibetan data refactor

--- src/tibetanUnicodeData.ts
+++ src/tibetanUnicodeData.ts
@@ -1,38 +1,173 @@
+export type TibetanSyllable = {
+	wylie: string;
+	phonetic: string;
+	// unicode: string;
+	column: Column;
+};
+
+export enum Column {
+	FIRST = 'FIRST',
+	SECOND = 'SECOND',
+	THIRD = 'THIRD',
+	FOURTH = 'FOURTH',
+	NONE = 'NONE',
+}
+
 export const superscripts: string[] = ['\u0F62', '\u0F63', '\u0F66'];
 
 export const prefixes: string[] = ['\u0F42', '\u0F51', '\u0F56', '\u0F58', '\u0F60'];
 
-export const roots: { [unicode: string]: string } = {
-	ཀ: 'ka',
-	ཅ: 'ca',
-	ཏ: 'ta',
-	པ: 'pa',
-	ཙ: 'tsa',
-	ཞ: 'zha',
-	ར: 'ra',
-	ཧ: 'ha',
-	ཁ: 'kha',
-	ཆ: 'cha',
-	ཐ: 'tha',
-	ཕ: 'pha',
-	ཚ: 'tsha',
-	ཟ: 'za',
-	ལ: 'la',
-	ཨ: 'a',
-	ག: 'kha',
-	ཇ: 'cha',
-	ད: 'tha',
-	བ: 'pha',
-	ཛ: 'dza',
-	འ: 'a',
-	ཤ: 'sha',
-	ང: 'nga',
-	ཉ: 'nya',
-	ན: 'na',
-	མ: 'ma',
-	ཝ: 'wa',
-	ཡ: 'ya',
-	ས: 'sa',
+export const roots: { [unicode: string]: TibetanSyllable } = {
+	ཀ: {
+		wylie: 'ka',
+		phonetic: 'ka',
+		column: Column.FIRST,
+	},
+	ཅ: {
+		wylie: 'ca',
+		phonetic: 'ca',
+		column: Column.FIRST,
+	},
+	ཏ: {
+		wylie: 'ta',
+		phonetic: 'ta',
+		column: Column.FIRST,
+	},
+	པ: {
+		wylie: 'pa',
+		phonetic: 'pa',
+		column: Column.FIRST,
+	},
+	ཙ: {
+		wylie: 'tsa',
+		phonetic: 'tsa',
+		column: Column.FIRST,
+	},
+	ཞ: {
+		wylie: 'zha',
+		phonetic: 'zha',
+		column: Column.NONE,
+	},
+	ར: {
+		wylie: 'ra',
+		phonetic: 'ra',
+		column: Column.NONE,
+	},
+	ཧ: {
+		wylie: 'ha',
+		phonetic: 'ha',
+		column: Column.NONE,
+	},
+	ཁ: {
+		wylie: 'kha',
+		phonetic: 'kha',
+		column: Column.SECOND,
+	},
+	ཆ: {
+		wylie: 'cha',
+		phonetic: 'cha',
+		column: Column.SECOND,
+	},
+	ཐ: {
+		wylie: 'tha',
+		phonetic: 'tha',
+		column: Column.SECOND,
+	},
+	ཕ: {
+		wylie: 'pha',
+		phonetic: 'pha',
+		column: Column.SECOND,
+	},
+	ཚ: {
+		wylie: 'tsha',
+		phonetic: 'tsha',
+		column: Column.SECOND,
+	},
+	ཟ: {
+		wylie: 'za',
+		phonetic: 'za',
+		column: Column.NONE,
+	},
+	ལ: {
+		wylie: 'la',
+		phonetic: 'la',
+		column: Column.NONE,
+	},
+	ཨ: {
+		wylie: 'a',
+		phonetic: 'a',
+		column: Column.NONE,
+	},
+	ག: {
+		wylie: 'ga',
+		phonetic: 'kha',
+		column: Column.THIRD,
+	},
+	ཇ: {
+		wylie: 'ja',
+		phonetic: 'cha',
+		column: Column.THIRD,
+	},
+	ད: {
+		wylie: 'da',
+		phonetic: 'tha',
+		column: Column.THIRD,
+	},
+	བ: {
+		wylie: 'ba',
+		phonetic: 'pha',
+		column: Column.THIRD,
+	},
+	ཛ: {
+		wylie: 'dza',
+		phonetic: 'dza',
+		column: Column.THIRD,
+	},
+	འ: {
+		wylie: "'a",
+		phonetic: 'a',
+		column: Column.NONE,
+	},
+	ཤ: {
+		wylie: 'sha',
+		phonetic: 'sha',
+		column: Column.NONE,
+	},
+	ང: {
+		wylie: 'nga',
+		phonetic: 'nga',
+		column: Column.FOURTH,
+	},
+	ཉ: {
+		wylie: 'nya',
+		phonetic: 'nya',
+		column: Column.FOURTH,
+	},
+	ན: {
+		wylie: 'na',
+		phonetic: 'na',
+		column: Column.FOURTH,
+	},
+	མ: {
+		wylie: 'ma',
+		phonetic: 'ma',
+		column: Column.FOURTH,
+	},
+	ཝ: {
+		wylie: 'wa',
+		phonetic: 'wa',
+		column: Column.NONE, // TODO: FOURTH or NONE?
+	},
+	ཡ: {
+		wylie: 'ya',
+		phonetic: 'ya',
+		column: Column.NONE,
+	},
+	ས: {
+		wylie: 'sa',
+		phonetic: 'sa',
+		column: Column.NONE,
+	},
 };
 
 export const modifiedThirdColumn: { [unicode: string]: string } = {
