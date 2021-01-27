f5437fe6656db03bb20af0172b9d8d05ff303f4f
ak4ra <aryjna1@gmail.com>
2021-01-27 15:52:30 +0200

    refactored to functional component with reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import TibetanCharacterMenu from './TibetanCharacterMenu';
 import {
 	prefixes,
@@ -12,11 +12,9 @@ import {
 	Tone,
 } from './tibetanData';
 
-const DIAIRESIS_UNICODE_CODEPOINT = '\u0308';
-const HIGH_TONE_UNICODE_CODEPOINT = '\u0301';
-const LOW_TONE_UNICODE_CODEPOINT = '\u0300';
-
-type AppProps = null;
+const DIAIRESIS_UNICODE_CODE_POINT = '\u0308';
+const HIGH_TONE_UNICODE_CODE_POINT = '\u0301';
+const LOW_TONE_UNICODE_CODE_POINT = '\u0300';
 
 interface AppState {
 	root: string;
@@ -28,126 +26,171 @@ interface AppState {
 	availableSubscripts: string[];
 }
 
-// manual of standard tibetan p 44
-class App extends Component<AppProps, AppState> {
-	state: AppState = {
-		root: '',
-		superscript: '',
-		prefix: '',
-		suffix: '',
-		secondSuffix: '',
-		subscript: '',
-		availableSubscripts: [],
-	};
+const initialState: AppState = {
+	root: '',
+	superscript: '',
+	prefix: '',
+	suffix: '',
+	secondSuffix: '',
+	subscript: '',
+	availableSubscripts: [],
+};
 
-	handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+enum AppStateActionType {
+	CLEAR_STATE,
+	UPDATE_PREFIX,
+	UPDATE_SUPERSCRIPT,
+	UPDATE_ROOT,
+	UPDATE_SUBSCRIPT,
+	UPDATE_SUFFIX,
+	UPDATE_SECOND_SUFFIX,
+}
+
+interface AppStateAction {
+	type: AppStateActionType;
+	payload: string;
+}
+
+function reducer(state: AppState, action: AppStateAction): AppState {
+	switch (action.type) {
+		case AppStateActionType.CLEAR_STATE:
+			return initialState;
+		case AppStateActionType.UPDATE_PREFIX:
+			return {
+				...state,
+				prefix: action.payload,
+			};
+		case AppStateActionType.UPDATE_SUPERSCRIPT:
+			// the la subscript cannot be displayed if there is a superscript
+			// remove la from the subscripts menu if a superscript is selected and clear current subscript
+			if (action.payload) {
+				const prunedSubscripts = state.availableSubscripts.filter((s) => s !== 'ལ');
+				return {
+					...state,
+					superscript: action.payload,
+					availableSubscripts: prunedSubscripts,
+					subscript: '',
+				};
+			}
+			// if the superscript is deselected, load all subscripts for the current root
+			if (subscriptsTable[state.root]) {
+				return {
+					...state,
+					availableSubscripts: [...subscriptsTable[state.root]],
+					superscript: action.payload,
+				};
+			}
+			return {
+				...state,
+				superscript: action.payload,
+			};
+		case AppStateActionType.UPDATE_ROOT:
+			// prefixes, superscripts, and suffixes are available for all roots,
+			// specific subscripts are available to specific roots
+			return {
+				...initialState,
+				root: action.payload,
+				availableSubscripts: [...subscriptsTable[action.payload]],
+			};
+		case AppStateActionType.UPDATE_SUBSCRIPT:
+			return {
+				...state,
+				subscript: action.payload,
+			};
+		case AppStateActionType.UPDATE_SUFFIX:
+			return {
+				...state,
+				suffix: action.payload,
+				secondSuffix: '',
+			};
+		case AppStateActionType.UPDATE_SECOND_SUFFIX:
+			return {
+				...state,
+				secondSuffix: action.payload,
+			};
+		default:
+			throw new Error();
+	}
+}
+
+// manual of standard tibetan p 44
+function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
 		const { name, value } = event.target;
 		switch (name) {
-			case 'root':
-				this.setState({
-					...this.state,
-					[name]: value,
-				});
-				this.handleRootChange(value);
-				break;
-			case 'suffix':
-				this.setState({
-					...this.state,
-					secondSuffix: '',
-					[name]: value,
+			case 'prefix':
+				dispatch({
+					type: AppStateActionType.UPDATE_PREFIX,
+					payload: value,
 				});
 				break;
 			case 'superscript':
-				// the la subscript cannot be displayed if there is a superscript
-				// remove la from the subscripts menu if a superscript is selected and clear current subscript
-				if (value) {
-					const prunedSubscripts = this.state.availableSubscripts.filter((s) => s !== 'ལ');
-					this.setState({
-						...this.state,
-						[name]: value,
-						availableSubscripts: prunedSubscripts,
-						subscript: '',
-					});
-					break;
-				}
-				// if the superscript is deselected, load all subscripts for the current root
-				if (subscriptsTable[this.state.root]) {
-					this.setState({
-						...this.state,
-						availableSubscripts: [...subscriptsTable[this.state.root]],
-						[name]: value,
-					});
-				} else {
-					this.setState({
-						...this.state,
-						[name]: value,
-					});
-				}
-				break;
-			default:
-				this.setState({
-					...this.state,
-					[name]: value,
+				dispatch({
+					type: AppStateActionType.UPDATE_SUPERSCRIPT,
+					payload: value,
 				});
 				break;
+			case 'root':
+				dispatch({
+					type: AppStateActionType.UPDATE_ROOT,
+					payload: value,
+				});
+				break;
+			case 'subscript':
+				dispatch({
+					type: AppStateActionType.UPDATE_SUBSCRIPT,
+					payload: value,
+				});
+				break;
+			case 'suffix':
+				dispatch({
+					type: AppStateActionType.UPDATE_SUFFIX,
+					payload: value,
+				});
+				break;
+			case 'secondSuffix':
+				dispatch({
+					type: AppStateActionType.UPDATE_SECOND_SUFFIX,
+					payload: value,
+				});
+				break;
+			default:
+				break;
 		}
 	};
 
-	handleRootChange = (value: string): void => {
-		// prefixes, superscripts, and suffixes are available for all roots,
-		// specific subscripts are available to specific roots
-
-		// reset the affixes in state
-		this.setState({
-			superscript: '',
-			prefix: '',
-			suffix: '',
-			secondSuffix: '',
-			subscript: '',
-			availableSubscripts: [],
-		});
-
-		if (subscriptsTable[value]) {
-			this.setState({
-				availableSubscripts: [...subscriptsTable[value]],
-			});
-		}
-	};
-
-	createTibetanDisplay = (): string => {
-		if (!this.state.root) {
+	const createTibetanDisplay = (): string => {
+		if (!state.root) {
 			return 'ཨ';
 		}
-		const prefix = this.state.prefix ? roots[this.state.prefix].unicodeCodePoint : '';
-		const superscript = this.state.superscript ? roots[this.state.superscript].unicodeCodePoint : '';
-		const subscript = this.state.subscript
-			? roots[this.state.subscript].unicodeCodePointAsSubscript
-			: '';
-		const suffix = this.state.suffix ? roots[this.state.suffix].unicodeCodePoint : '';
-		const secondSuffix = this.state.secondSuffix
-			? roots[this.state.secondSuffix].unicodeCodePoint
-			: '';
+		const prefix = state.prefix ? roots[state.prefix].unicodeCodePoint : '';
+		const superscript = state.superscript ? roots[state.superscript].unicodeCodePoint : '';
+		const subscript = state.subscript ? roots[state.subscript].unicodeCodePointAsSubscript : '';
+		const suffix = state.suffix ? roots[state.suffix].unicodeCodePoint : '';
+		const secondSuffix = state.secondSuffix ? roots[state.secondSuffix].unicodeCodePoint : '';
 		const root = superscript
-			? roots[this.state.root].unicodeCodePointAsSubscript
-			: roots[this.state.root].unicodeCodePoint;
+			? roots[state.root].unicodeCodePointAsSubscript
+			: roots[state.root].unicodeCodePoint;
 		return `${prefix}${superscript}${root}${subscript}${suffix}${secondSuffix}`;
 	};
 
-	createPhoneticDisplay = (): string => {
-		if (!this.state.root || !roots[this.state.root]) {
+	const createPhoneticDisplay = (): string => {
+		if (!state.root || !roots[state.root]) {
 			return '';
 		}
 
-		const root = roots[this.state.root];
+		const root = roots[state.root];
 		let rootPhonetic = root.phonetic;
-		const column = roots[this.state.root].column;
+		const column = roots[state.root].column;
 		let tone: Tone = Tone.NONE;
 		let diairesis = false;
 		let suffix = '';
 
 		// determine if there is change in the root, based on a prefix, superscript, or subscript
 		// the root change of the subscript overrides the root change for a third column root with prefix
-		if (this.state.prefix || this.state.superscript) {
+		if (state.prefix || state.superscript) {
 			if (column === Column.THIRD) {
 				rootPhonetic = root.phoneticModifiedThirdColumn
 					? root.phoneticModifiedThirdColumn
@@ -158,33 +201,33 @@ class App extends Component<AppProps, AppState> {
 			}
 		}
 
-		if (this.state.subscript) {
-			[rootPhonetic, tone] = this.subscriptPhoneticChange(
-				this.state.subscript,
-				this.state.root,
+		if (state.subscript) {
+			[rootPhonetic, tone] = subscriptPhoneticChange(
+				state.subscript,
+				state.root,
 				rootPhonetic,
-				this.state.superscript,
+				state.superscript,
 				tone
 			);
 		}
 
-		if (this.state.suffix) {
-			if (suffixesThatCauseVowelChange.includes(this.state.suffix)) {
+		if (state.suffix) {
+			if (suffixesThatCauseVowelChange.includes(state.suffix)) {
 				diairesis = true;
 			}
-			suffix = roots[this.state.suffix].phoneticAsSuffix || '';
+			suffix = roots[state.suffix].phoneticAsSuffix || '';
 		}
 
 		let phoneticDisplay = rootPhonetic;
-		if (diairesis) phoneticDisplay += DIAIRESIS_UNICODE_CODEPOINT;
-		if (tone === Tone.HIGH) phoneticDisplay += HIGH_TONE_UNICODE_CODEPOINT;
-		if (tone === Tone.LOW) phoneticDisplay += LOW_TONE_UNICODE_CODEPOINT;
+		if (diairesis) phoneticDisplay += DIAIRESIS_UNICODE_CODE_POINT;
+		if (tone === Tone.HIGH) phoneticDisplay += HIGH_TONE_UNICODE_CODE_POINT;
+		if (tone === Tone.LOW) phoneticDisplay += LOW_TONE_UNICODE_CODE_POINT;
 		phoneticDisplay += suffix;
 
 		return phoneticDisplay;
 	};
 
-	subscriptPhoneticChange(
+	function subscriptPhoneticChange(
 		subscript: string,
 		root: string,
 		rootPhonetic: string,
@@ -267,88 +310,86 @@ class App extends Component<AppProps, AppState> {
 		return [rootPhonetic, tone];
 	}
 
-	render() {
-		const MENUS = [
-			// prefixes
-			{
-				active: Boolean(this.state.root),
-				handleChange: this.handleChange,
-				label: 'Prefix',
-				identifier: 'prefix',
-				value: this.state.prefix,
-				options: prefixes,
-			},
-			// superscripts
-			{
-				active: Boolean(this.state.root),
-				handleChange: this.handleChange,
-				label: 'Superscript',
-				identifier: 'superscript',
-				value: this.state.superscript,
-				options: superscripts,
-			},
-			// root characters menu
-			{
-				active: true,
-				handleChange: this.handleChange,
-				label: 'Root character',
-				identifier: 'root',
-				value: this.state.root,
-				options: Object.keys(roots),
-			},
-			// subscripts
-			{
-				active: this.state.availableSubscripts.length > 0,
-				handleChange: this.handleChange,
-				label: 'Subscript',
-				identifier: 'subscript',
-				value: this.state.subscript,
-				options: this.state.availableSubscripts,
-			},
-			// suffixes
-			{
-				active: Boolean(this.state.root),
-				handleChange: this.handleChange,
-				label: 'Suffix 1',
-				identifier: 'suffix',
-				value: this.state.suffix,
-				options: suffixes,
-			},
-			// second suffixes
-			{
-				active: Boolean(this.state.suffix),
-				handleChange: this.handleChange,
-				label: 'Suffix 2',
-				identifier: 'secondSuffix',
-				value: this.state.secondSuffix,
-				options: secondSuffixes,
-			},
-		];
-		return (
-			<div className="container">
-				{/* tibetan display */}
-				<div className="display--tibetan">
-					<h1>{this.createTibetanDisplay()}</h1>
-				</div>
-				{/* transliteration display */}
-				<div className="display--transliteration">{this.createPhoneticDisplay()}</div>
-				{/* character menus */}
-				<div className="options">
-					{MENUS.map((m, i) => (
-						<TibetanCharacterMenu
-							key={`menu_${i}`}
-							active={m.active}
-							handleChange={m.handleChange}
-							label={m.label}
-							identifier={m.identifier}
-							value={m.value}
-							options={m.options}
-						/>
-					))}
-				</div>
+	const MENUS = [
+		// prefixes
+		{
+			active: Boolean(state.root),
+			handleChange: handleChange,
+			label: 'Prefix',
+			identifier: 'prefix',
+			value: state.prefix,
+			options: prefixes,
+		},
+		// superscripts
+		{
+			active: Boolean(state.root),
+			handleChange: handleChange,
+			label: 'Superscript',
+			identifier: 'superscript',
+			value: state.superscript,
+			options: superscripts,
+		},
+		// root characters menu
+		{
+			active: true,
+			handleChange: handleChange,
+			label: 'Root character',
+			identifier: 'root',
+			value: state.root,
+			options: Object.keys(roots),
+		},
+		// subscripts
+		{
+			active: state.availableSubscripts.length > 0,
+			handleChange: handleChange,
+			label: 'Subscript',
+			identifier: 'subscript',
+			value: state.subscript,
+			options: state.availableSubscripts,
+		},
+		// suffixes
+		{
+			active: Boolean(state.root),
+			handleChange: handleChange,
+			label: 'Suffix 1',
+			identifier: 'suffix',
+			value: state.suffix,
+			options: suffixes,
+		},
+		// second suffixes
+		{
+			active: Boolean(state.suffix),
+			handleChange: handleChange,
+			label: 'Suffix 2',
+			identifier: 'secondSuffix',
+			value: state.secondSuffix,
+			options: secondSuffixes,
+		},
+	];
+	return (
+		<div className="container">
+			{/* tibetan display */}
+			<div className="display--tibetan">
+				<h1>{createTibetanDisplay()}</h1>
 			</div>
-		);
-	}
+			{/* transliteration display */}
+			<div className="display--transliteration">{createPhoneticDisplay()}</div>
+			{/* character menus */}
+			<div className="options">
+				{MENUS.map((m, i) => (
+					<TibetanCharacterMenu
+						key={`menu_${i}`}
+						active={m.active}
+						handleChange={m.handleChange}
+						label={m.label}
+						identifier={m.identifier}
+						value={m.value}
+						options={m.options}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
